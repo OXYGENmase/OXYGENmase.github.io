f9086fe44a26e0497b03d6d2e674304ee83e57c4
mason <masoncarterallen09@outlook.com>
2025-08-29 10:09:54 +0100

    Update config for OXYGENmase.github.io

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['lucide-react'],
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/OXYGENmase-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/OXYGENmase-portfolio/' : '',
   trailingSlash: true,
   output: 'export',
   images: {
